c28f0c00fb2d06eef174db3cbbb3136285879bdf
swit22 <switkin1@jhu.edu>
2018-06-04 10:40:53 +0200

    Simplified, used indices

--- VertexConcrete.py
+++ VertexConcrete.py
@@ -9,44 +9,34 @@ Abstract Min vertex cover setup
 from __future__ import division
 import pandas as pd
 import numpy as np
-from pyomo.core import *
 from pyomo.environ import *
 from pyomo.opt import SolverFactory
 opt= SolverFactory('glpk')
 
 
 model = ConcreteModel()
-A = pd.read_csv('book2.csv')
+A = pd.read_csv('Graph2.csv')
 
-#model.I = Set(initialize=A.index)
-#model.J = Set(initialize=A.columns)
-#model.x = Var(range(len(A.columns)),domain = Boolean)
+model.I = Set(initialize=A.index)
+model.J = Set(initialize=A.columns)
 
 
-model.x = Var(A.columns,domain = Boolean)
-
+model.x = Var(model.J,domain = Boolean, initialize = 1)
+  
 #A vector of ones
-model.c = Param(A.columns, initialize = {'a':1,'b':1,'c':1,'d':1,'e':1,'f':1,'g':1})
+#model.c = Param(A.columns, initialize = {'a':1,'b':1,'c':1,'d':1,'e':1,'f':1,'g':1})
+model.c = Param(model.J, initialize = 1)
 
-#initializes them to 1, for some reason it needs initialization
-for i in A.columns:
-    model.x[i] = 1
-
-
-def obj_expression(model):
-    return summation(model.x, model.c, index = A.columns) #c'x
-
+#Below returns a constant epression. No good. Not sure how defining sums works in rules/ variables.
+#def obj_expression(model):
+#    return summation(model.x, model.c, index = model.J) #c'x
 #model.OBJ = Objective(rule = obj_expression(model))
-model.OBJ = Objective(expr = summation(model.x, model.c, index = A.columns))
 
+model.OBJ = Objective(expr = summation(model.x, model.c, index = model.J))
 
 def ax_constraint_rule(model,i):
     return summation(A.iloc[i,:],model.x) >= 1
-model.AxbConstraint = Constraint(range(9), rule = ax_constraint_rule)
-
+model.AxbConstraint = Constraint(model.I, rule = ax_constraint_rule)
 
 results = opt.solve(model)
-
-
 print(model.display())
-
